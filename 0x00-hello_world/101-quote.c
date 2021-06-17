e6ba8c7ed5cabe979af82e79a85d21ae5d6be278
Pascal Madiri <psclmadiri9@gmail.com>
2021-06-17 20:27:18 +0000

    Edited script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19"):
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
