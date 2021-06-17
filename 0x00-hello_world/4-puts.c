c88eadb5e4a932d7d4da983531592570fc1137d8
Pascal Madiri <psclmadiri9@gmail.com>
2021-06-17 17:47:14 +0000

    Added Betty Documentation style on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-/*
- * C program that prints exactly
+/**
+ * main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
