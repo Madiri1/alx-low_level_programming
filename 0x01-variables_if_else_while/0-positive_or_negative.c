520a6dedb23682dc926381ca9cd8a1a82ba6a2a1
Pascal Madiri <psclmadiri9@gmail.com>
2021-06-18 19:27:18 +0000

    betty style formating

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive\n", n);
+		printf("is positive");
 	else if (n == 0)
-		printf("%d is zero\n", n);
+		printf("is zero");
 	else
-		printf("%d is negative\n, n");
+		printf("is negative");
 
 	return (0);
 }
