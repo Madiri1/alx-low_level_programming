b5e634471ae8513de2b488ed263ec78a97adf129
Pascal Madiri <psclmadiri9@gmail.com>
2021-06-18 18:40:28 +0000

    Formatted the file in betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,24 +3,31 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - check if number stored in the variable n is positive or negative.
+ *
+ * Description: program will assign a random number to variable n each time
+ * it is executed.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-  if (n > 0)
-  {
-    printf("is positive");
-  }
-  else if (n == 0)
-  {
-    printf("is zero");
-  }
-  else {
-    printf("is negative");
-  }
+	if (n > 0)
+	{
+		printf("is positive");
+	}
+	else if (n == 0)
+	{
+		printf("is zero");
+	}
+	else
+	{
+		printf("is negative");
+	}
 	return (0);
 }
