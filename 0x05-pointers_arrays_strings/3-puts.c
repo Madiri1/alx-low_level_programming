f69806626adead77e2c815deb1ce4add6c8744ab
Pascal Madiri <psclmadiri9@gmail.com>
2021-07-04 12:54:25 +0000

    a function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+# include "holberton.h"
+
+/**
+ *_puts - a function that prints a string
+ *
+ * @str: string 
+ *
+ * Return: void
+ */
+
+_puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != "\0")
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar(\n);
+}
