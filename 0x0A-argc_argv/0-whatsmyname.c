01eaf3b22d08a03244e67a00ed00cef7fdda2d93
Pascal Madiri <psclmadiri9@gmail.com>
2021-07-12 19:26:46 +0000

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,18 @@
+
 #include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - Prints name of file
- * @argc: number of arguments
- * @argv:array of arguments
- * Return: 0
- */
-
-int main(__attribute__((unused)) int argc, char *argv[])
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
 {
-	printf("%s", argv[0]);
+	(void) argc;
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
