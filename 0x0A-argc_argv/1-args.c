b85eee369c882800fac4c42f1d88e75e237e41b7
Pascal Madiri <psclmadiri9@gmail.com>
2021-07-12 19:33:53 +0000

    program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdiio.h>
+
+/** main -number of arguments
+ * @argc: first parameter
+ * @argv: second parameter
+ * return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d", argc);
+	return (0);
+}
