9f7bade91c7ef6ef2352a1b3f7e6668966447a4a
Pascal Madiri <psclmadiri9@gmail.com>
2021-07-12 15:15:58 +0000

    function that prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
 
 /**
- * _puts - prints string
- * @str: string
- * Return: void
+ * _puts - put string
+ * @str: first parameter
+ *
+ * Description: prints a string
+ * Return: Alwalys (0)
  */
 
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str);
+		str++;
 	}
-
 	_putchar('\n');
 }
