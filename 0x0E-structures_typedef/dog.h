adbd65e990e62a57a4b456255cbbfe187865ad46
Pascal Madiri <psclmadiri9@gmail.com>
2021-07-20 01:19:10 +0000

    initialize a variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,6 @@ int _putchar(char c);
  * @owner: owner's name
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog
 {
 	char *name;
@@ -16,4 +15,5 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* !DOG_H */
